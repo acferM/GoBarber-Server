d17a5714000ec0257faf379db9f21d2e9202a7c3
acferM <matheus.acfer@nave.org.br>
2021-01-12 02:54:13 -0300

    config to heroku#3

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -40,6 +40,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   });
 });
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log('🚀 Server started on port 3333!');
 });
