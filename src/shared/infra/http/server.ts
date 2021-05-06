0e26bf41f3112d305eb08f8285539eb87bf4de7d
acferM <matheus.acfer@nave.org.br>
2021-05-06 12:44:29 -0300

    Update server.ts

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -16,7 +16,9 @@ import '@shared/container';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://gobarber',
+}));
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
 app.use(rateLimiter);
